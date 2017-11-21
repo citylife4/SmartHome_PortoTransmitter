efaabf4d0d85a0a8e8c9745625900a76e9222b81
José Valverde <delfimvalverde@gmail.com>
2017-11-21 22:53:19 +0000

    Updated, now fully functional.
    To be updated so that send socket is not in this process

--- Socket/socket_connection.py
+++ Socket/socket_connection.py
@@ -14,53 +14,66 @@ connected = False
 def waiter_receive_socket(condition, lol, threadName):
     # Bind the socket to the port
     global connected
+    connected = False
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ('', 4662)
+    logging.info('waiter_receive_socket - Starting receiver socket up on %s port %s' % server_address)
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
     while 1:
-        condition.acquire()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        server_address = ('', 4662)
-        logging.info('waiter_receive_socket - Starting receiver socket up on %s port %s' % server_address)
-        sock.bind(server_address)
-        # Listen for incoming connections
-        sock.listen(1)
         connection, client_address = sock.accept()
-
+        # condition.acquire()
         try:
-            logging.info('waiter_receive_socket - Received connection from ' + client_address[0])
+            logging.info('Received connection from ' + client_address[0])
             # Receive the data in small chunks and retransmit it
-            data = connection.recv(16)
-            logging.info('waiter_receive_socket - Received %s' % data)
-            ip_file = open("ip_file.txt", "w")
-            ip_file.write(client_address[0])
-            ip_file.close()
-            condition.notify()
-            connected = True
-            condition.release()
+            data = connection.recv(1024).decode()
+            logging.info('Received %s' % data)
+            if "IP" in data:
+                logging.warning("IP received")
+                connected = False
+                ip_file = open("ip_file.txt", "w")
+                ip_file.write(client_address[0])
+                ip_file.close()
+                # condition.notify()
+                connected = True
+                # condition.release()
+            elif "status" in data:
+                logging.warning("Not Implemented %s" % data)
 
         finally:
             # Clean up the connection.
-            logging.info('waiter_receive_socket - Cleaning the connection')
+            logging.info('Cleaning the connection')
             connection.close()
-            sock.close()
-            time.sleep(1000)
-            connected = False
 
+    # For shits and gigles
+    sock.close()
+
+"""
+
+This will be kept in the Webserver proto
+
+"""
 
 def send_socket(condition, lol, threadName):
     global connected
 
-    time.sleep(random.randrange(2, 5))  # Sleeps for some time.
-    logging.info('send_socket - aquiring lock')
+    # time.sleep(random.randrange(2, 5))  # Sleeps for some time.
+    logging.info('Aquiring lock')
     # condition.acquire()
-    logging.info('send_socket - Waiting for connection %s' % connected)
+    logging.info('Waiting for connection %s' % connected)
 
-    with condition:
-        while not connected:
-            condition.wait()
+    # TODO: check how to make with conditions....
+    while not connected:
+        time.sleep(10)
 
-    logging.info('send_socket - Connected %s' % connected)
+    #with condition:
+    #    condition.wait_for(connected)
+
+    logging.info('Connected %s' % connected)
 
     if connected:
         sock_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -72,12 +85,12 @@ def send_socket(condition, lol, threadName):
         ip_file.close()
 
         server_address = (client_address, 45321)
-        logging.info('send_socket - Connecting Raspberry to %s on %s' % server_address)
+        logging.info('Connecting Raspberry to %s on %s' % server_address)
         sock_send.connect(server_address)
         try:
             message = "open"
             sock_send.sendall(message.encode('utf-8'))
-            data_door = sock_send.recv(1024)
-            logging.info('send_socket - Received %s ' % data_door)
+            data_door = sock_send.recv(1024).decode()
+            logging.info('Received %s ' % data_door)
         finally:
             sock_send.close()
