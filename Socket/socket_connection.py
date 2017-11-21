cf1b3639eeb5557db8f2f307eecace22bf504012
José Valverde <delfimvalverde@gmail.com>
2017-11-21 22:54:40 +0000

    Removed def to be send

--- Socket/socket_connection.py
+++ Socket/socket_connection.py
@@ -38,9 +38,7 @@ def waiter_receive_socket(condition, lol, threadName):
                 ip_file = open("ip_file.txt", "w")
                 ip_file.write(client_address[0])
                 ip_file.close()
-                # condition.notify()
                 connected = True
-                # condition.release()
             elif "status" in data:
                 logging.warning("Not Implemented %s" % data)
 
@@ -51,46 +49,3 @@ def waiter_receive_socket(condition, lol, threadName):
 
     # For shits and gigles
     sock.close()
-
-"""
-
-This will be kept in the Webserver proto
-
-"""
-
-def send_socket(condition, lol, threadName):
-    global connected
-
-    # time.sleep(random.randrange(2, 5))  # Sleeps for some time.
-    logging.info('Aquiring lock')
-    # condition.acquire()
-    logging.info('Waiting for connection %s' % connected)
-
-    # TODO: check how to make with conditions....
-    while not connected:
-        time.sleep(10)
-
-    #with condition:
-    #    condition.wait_for(connected)
-
-    logging.info('Connected %s' % connected)
-
-    if connected:
-        sock_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        # Connect the socket to the port where the server is listening
-        ip_file = open("ip_file.txt", "r")
-        client_address = ip_file.read()
-        ip_file.close()
-
-        server_address = (client_address, 45321)
-        logging.info('Connecting Raspberry to %s on %s' % server_address)
-        sock_send.connect(server_address)
-        try:
-            message = "open"
-            sock_send.sendall(message.encode('utf-8'))
-            data_door = sock_send.recv(1024).decode()
-            logging.info('Received %s ' % data_door)
-        finally:
-            sock_send.close()
