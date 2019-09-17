7e0308f01a1f4a482b01b9fe048e4b4632e1ab0e
Jose Valverde <delfimvalverde@gmail.com>
2019-09-17 23:54:19 +0100

    added small test

---
+++ tests/test_base.py
@@ -0,0 +1,27 @@
+from homedash import app
+import unittest
+
+
+class FlaskAppTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        assert 1 == 1
